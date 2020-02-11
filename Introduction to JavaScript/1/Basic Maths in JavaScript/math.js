f5f7cfb1baf1b3b805ab920f7d8dd7c2de8f697f
Akash Jeganath <akashjoffl@icloud.com>
2020-02-11 23:22:03 +0530

    Strings 1

--- Introduction to JavaScript/1/Basic Maths in JavaScript/math.js
+++ Introduction to JavaScript/1/Basic Maths in JavaScript/math.js
@@ -1,32 +1,4 @@
-// Statement 1: The elephant weights less than the mouse
-let eleWeight = 1000;
-let mouseWeight = 2;
-let weightComparison;
+// Add your code here
 
-if (eleWeight > mouseWeight) {
-    weightComparison = "The elephant weights more than the mouse"
-} else {
-    "The elephant weights less than the mouse"
-};
-
-
-// Statement 2: The Ostrich is taller than the duck
-let ostrichHeight = 2;
-let duckHeight = 0.3;
-let heightComparison;
-
-if (ostrichHeight > duckHeight) {
-    heightComparison = "The Ostrich is taller than the duck"
-} else { "The duck is taller than the Ostrich" };
-
-
-// Statement 3: The two passwords match
-let pwd1 = 'stromboli';
-let pwd2 = 'stROmBoLi'
-let pwdMatch;
-
-if (pwd1 === pwd2) {
-    pwdMatch = "The two passwords match"
-} else {
-    "The two passwords do not match"
-};
+let quoteStart = 'Don\'t judge each day by the harvest you reap ';
+let finalQuote = quoteStart;
