6febbb59a4f7f932159d39784193d96a172e8f7c
Akash Jeganath <akashjoffl@icloud.com>
2020-02-11 23:11:43 +0530

    Math 3

--- Introduction to JavaScript/1/Basic Maths in JavaScript/math.js
+++ Introduction to JavaScript/1/Basic Maths in JavaScript/math.js
@@ -1,11 +1,32 @@
-// Final result should be 10.42
-// Add/update your code here
+// Statement 1: The elephant weights less than the mouse
+let eleWeight = 1000;
+let mouseWeight = 2;
+let weightComparison;
 
-let result = 7 + 13 / 9 + 7;
-let result2 = 100 / 2 * 6;
+if (eleWeight > mouseWeight) {
+    weightComparison = "The elephant weights more than the mouse"
+} else {
+    "The elephant weights less than the mouse"
+};
 
-result = result * result2;
-let finalResult = result.toFixed(2);
 
-finalNumber = Number(finalResult);
-console.log(typeof(finalNumber));
+// Statement 2: The Ostrich is taller than the duck
+let ostrichHeight = 2;
+let duckHeight = 0.3;
+let heightComparison;
+
+if (ostrichHeight > duckHeight) {
+    heightComparison = "The Ostrich is taller than the duck"
+} else { "The duck is taller than the Ostrich" };
+
+
+// Statement 3: The two passwords match
+let pwd1 = 'stromboli';
+let pwd2 = 'stROmBoLi'
+let pwdMatch;
+
+if (pwd1 === pwd2) {
+    pwdMatch = "The two passwords match"
+} else {
+    "The two passwords do not match"
+};
