7f1e1d81213e297cd2096a6950bc26260d07fb03
Akash Jeganath <akashjoffl@icloud.com>
2020-02-11 22:09:25 +0530

    Basic Maths 1

---
+++ Introduction to JavaScript/1/Basic Maths in JavaScript/math.js
@@ -0,0 +1,21 @@
+// Add your code here
+
+let a = 5;
+let b = 7;
+let c = 4;
+let d = 8;
+
+let sum = a + b;
+let sub = d - c;
+
+let finalResult = sum * sub;
+let evenOddCheck;
+
+if (finalResult % 2 === 0) {
+    evenOddCheck = "Even Number"
+} else {
+    evenOddCheck = "Odd Number"
+};
+
+console.log(finalResult);
+console.log(evenOddCheck);
