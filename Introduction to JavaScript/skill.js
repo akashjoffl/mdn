29ead6270d66ed3c26dabf3c6b5979d57013f64a
Akash Jeganath <akashjoffl@icloud.com>
2020-02-12 20:13:37 +0530

    Making new strings from old parts

--- Introduction to JavaScript/skill.js
+++ Introduction to JavaScript/skill.js
@@ -1,20 +1,18 @@
 const list = document.querySelector('.output ul');
 list.innerHTML = '';
-let cities = ['lonDon', 'ManCHESTer', 'BiRmiNGHAM', 'liVERpoOL'];
+let stations = ['MAN675847583748sjt567654;Manchester Piccadilly',
+    'GNF576746573fhdg4737dh4;Greenfield',
+    'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
+    'SYB4f65hf75f736463;Stalybridge',
+    'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'
+];
 
-for (let i = 0; i < cities.length; i++) {
-    let input = cities[i];
-    let lower = input.toLowerCase(); // Convert the whole of the string contained in the input variable to lower case and store it in a new variable.
+for (let i = 0; i < stations.length; i++) {
+    let input = stations[i];
+    // write your code just below here   
 
-    let firstLetter = lower.slice(0, 1); // Grab the first letter of the string in this new variable and store it in another variable.
-
-    let capitalized = lower.replace(firstLetter, firstLetter.toUpperCase());
-    /* Using this latest variable as a substring, replace the first letter of the lowercase string with the first letter of the lowercase string changed to upper case.
-    Store the result of this replace procedure in another new variable. */
-
-    let result = capitalized; // Change the value of the result variable to equal to the final result, not the input
+    let result = input;
     let listItem = document.createElement('li');
     listItem.textContent = result;
     list.appendChild(listItem);
-
 }
