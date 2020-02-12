9eb20d676ca44e78952ec2ca0f77ca55df80b866
Akash Jeganath <akashjoffl@icloud.com>
2020-02-12 15:51:10 +0530

    Strings 2

--- Introduction to JavaScript/1/Basic Maths in JavaScript/math.js
+++
@@ -1,4 +0,0 @@
-// Add your code here
-
-let quoteStart = 'Don\'t judge each day by the harvest you reap ';
-let finalQuote = quoteStart;
---
+++ Introduction to JavaScript/skill.js
@@ -0,0 +1,9 @@
+let quote = 'I do not like green eggs and ham. I do not like them, Sam-I-Am.';
+let substring = 'green eggs and ham';
+
+// Add your code here
+
+let quoteLength = quote.length;
+let index = quote.indexOf(substring);
+quote.trim("I do not like green eggs and ham.");
+let revisedQuote = quote;
