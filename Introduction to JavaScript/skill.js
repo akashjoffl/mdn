f3f70572f4881c44b4384b886f7da925675dec42
Akash Jeganath <akashjoffl@icloud.com>
2020-02-12 19:41:32 +0530

    Updating parts of a string

--- Introduction to JavaScript/skill.js
+++ Introduction to JavaScript/skill.js
@@ -1,3 +1,7 @@
-let radData = 'My NaMe Is MuD';
-radData.toLowerCase();
-radData.toUpperCase();
+    let browserType = 'mozilla';
+
+    browserType.replace('moz', 'van'); // "vanilla"
+    console.log(browserType); // mozilla
+
+    browserType = browserType.replace('moz', 'van');
+    console.log(browserType); // vanilla
