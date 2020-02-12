72b2c63459b1895e0bdcab732ef6b2081a27c93f
Akash Jeganath <akashjoffl@icloud.com>
2020-02-12 20:22:19 +0530

    Solution: Making new strings from old parts

--- Introduction to JavaScript/skill.js
+++ Introduction to JavaScript/skill.js
@@ -9,10 +9,18 @@ let stations = ['MAN675847583748sjt567654;Manchester Piccadilly',
 
 for (let i = 0; i < stations.length; i++) {
     let input = stations[i];
-    // write your code just below here   
+    let code = input.slice(0, 3); // Extract the three-letter station code and store it in a new variable.
 
-    let result = input;
+    let semiC = input.indexOf(';'); // Find the character index number of the semicolon
+
+    let name = input.slice(semiC + 1);
+    /* Extract the human-readable station name using the semicolon character index number 
+       as a reference point, and store it in a new variable. */
+
+    let result = code + ': ' + name; // Concatenate the two new variables and a string literal to make the final string.
+
     let listItem = document.createElement('li');
-    listItem.textContent = result;
+    listItem.textContent = result; // Change the value of the result variable to equal to the final string, not the input. 
+
     list.appendChild(listItem);
 }
