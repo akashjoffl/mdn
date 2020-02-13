419bc8d60206a522ad8cda2f5c2aeb021e15857c
Akash Jeganath <akashjoffl@icloud.com>
2020-02-13 22:42:33 +0530

    Adding and removing array items - push()

--- Introduction to JavaScript/skill.js
+++ Introduction to JavaScript/skill.js
@@ -1,9 +1,6 @@
-let myData = 'Manchester,London,Liverpool,Birmingham,Leeds,Carlisle';
-let myArray = myData.split(',');
-myArray; // [ "Manchester", "London", "Liverpool", "Birmingham", "Leeds", "Carlisle" ]
+let myArray = ['Manchester', 'London', 'Liverpool', 'Birmingham', 'Leeds', 'Carlisle'];
 
-let myNewString = myArray.join(',');
-myNewString; // "Manchester,London,Liverpool,Birmingham,Leeds,Carlisle"
-
-let dogNames = ['Rocket', 'Flash', 'Bella', 'Slugger'];
-dogNames.toString(); // Rocket,Flash,Bella,Slugger
+myArray.push('Cardiff');
+myArray;
+myArray.push('Bradford', 'Brighton');
+myArray;
