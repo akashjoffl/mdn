9657deebfdef42971a495c8e3698d7a5951f27b8
Akash Jeganath <akashjoffl@icloud.com>
2020-02-13 22:43:22 +0530

    uodated push();

--- Introduction to JavaScript/skill.js
+++ Introduction to JavaScript/skill.js
@@ -4,3 +4,13 @@ myArray.push('Cardiff');
 myArray;
 myArray.push('Bradford', 'Brighton');
 myArray;
+
+let newLength = myArray.push('Bristol');
+myArray;
+newLength;
+
+myArray.pop();
+
+let removedItem = myArray.pop();
+myArray;
+removedItem;
