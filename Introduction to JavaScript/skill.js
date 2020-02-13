7dac04f44901bafe06a097593ff57d8c495db8f6
Akash Jeganath <akashjoffl@icloud.com>
2020-02-13 23:48:01 +0530

    Arrays: Top 5 searches

--- Introduction to JavaScript/skill.js
+++ Introduction to JavaScript/skill.js
@@ -1,26 +1,36 @@
 const list = document.querySelector('.output ul');
-const totalBox = document.querySelector('.output p');
-let total = 0;
+const searchInput = document.querySelector('.output input');
+const searchBtn = document.querySelector('.output button');
+
 list.innerHTML = '';
-totalBox.textContent = '';
 
-let products = ['Underpants:6.99',
-    'Socks:5.99',
-    'T-shirt:14.99',
-    'Trousers:31.99',
-    'Shoes:23.99'
-];
+let myHistory = [];
 
-for (let i = 0; i < products.length; i++) {
-    let subArray = products[i].split(':'); // let subArray = products[i].split(':');
-    let name = subArray[0]; // "Socks"
-    let price = Number(subArray[1]); // 5.99
-    total += price;
-    itemText = name + ' — $' + price; // "Socks — $5.99"
+searchBtn.onclick = function() {
+    // we will only allow a term to be entered if the search input isn't empty
+    if (searchInput.value !== '') {
+        // number 1
 
-    let listItem = document.createElement('li');
-    listItem.textContent = itemText;
-    list.appendChild(listItem);
+        // empty the list so that we don't display duplicate entries
+        // the display is regenerated every time a search term is entered.
+        list.innerHTML = '';
+
+        // loop through the array, and display all the search terms in the list
+        for (let i = 0; i < myHistory.length; i++) {
+            itemText = myHistory[i];
+            const listItem = document.createElement('li');
+            listItem.textContent = itemText;
+            list.appendChild(listItem);
+        }
+
+        // If the array length is 5 or more, remove the oldest search term
+        if (myHistory.length >= 5) {
+            // number 2
+
+        }
+
+        // empty the search input and focus it, ready for the next term to be entered
+        searchInput.value = '';
+        searchInput.focus();
+    }
 }
-
-totalBox.textContent = 'Total: $' + total.toFixed(2);
