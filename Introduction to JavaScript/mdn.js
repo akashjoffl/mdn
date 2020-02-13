1b951d5f7765bbb4b0c84947b1269c7dbdc77ac9
Akash Jeganath <akashjoffl@icloud.com>
2020-02-14 01:04:04 +0530

    Arrays 2

---
+++ Introduction to JavaScript/mdn.js
@@ -0,0 +1,13 @@
+let myString = 'Ryu+Ken+Chun-Li+Cammy+Guile+Sakura+Sagat+Juri';
+let myArray = myString.split("+");
+let arrayLength = myArray.length;
+let lastItem = myArray.pop();
+
+
+/* 
+
+Array: Ryu,Ken,Chun-Li,Cammy,Guile,Sakura,Sagat
+
+The length of the array is 8.
+
+The last item in the array is "Juri". */
--- Introduction to JavaScript/skill.js
+++
@@ -1,5 +0,0 @@
-// Add your code here
-let myArray = ["Zone24x7", "SyscoLabs", "99X", "Virtusa"]
-myArray.unshift("ismAPAC");
-
-// Array(5) [ "ismAPAC", "Zone24x7", "SyscoLabs", "99X", "Virtusa" ]
